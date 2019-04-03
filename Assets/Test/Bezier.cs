266df7489a5b7916a2fb4a3a566f69cb0c16f352
王天懿 <wty16@mails.tsinghua.edu.cn>
2019-04-04 00:39:42 +0800

    进行了一些拖尾效果的测试。

---
+++ Assets/Test/Bezier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Bezier : MonoBehaviour
+{
+	public Transform transforms;
+	public float duration = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	int num = transforms.childCount;
+    	Vector3 []wayPoints = new Vector3[num];
+    	for(int i = 0; i < num; i++)
+    		wayPoints[i] = transforms.GetChild(i).position;
+        transform.DOPath(wayPoints, duration, PathType.CubicBezier, PathMode.Ignore).SetEase(Ease.InSine).Play();
+    }
+
+}
