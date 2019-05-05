c0ee73e556cc43d58cd915e2d2c50afc06c1e531
xiashj <xiashj96@gmail.com>
2019-05-05 21:32:04 +0800

    apply input smoothing to better support touch

---
+++ Assets/Script/DeactivateAfterTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateAfterTime : MonoBehaviour
+{
+    public float time;
+    Coroutine c;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        c = StartCoroutine(Deactivate());
+    }
+
+    IEnumerator Deactivate()
+    {
+        yield return new WaitForSeconds(time);
+        gameObject.SetActive(false);
+    }
+}
