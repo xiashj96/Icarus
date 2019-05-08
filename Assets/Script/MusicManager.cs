118b07796de0bde94405b8bb08c6ac9d22cc19a7
xiashj <xiashj96@gmail.com>
2019-05-08 21:57:34 +0800

    music sync is working now

--- Assets/Script/MusicManager.cs
+++ Assets/Script/MusicManager.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 [System.Serializable]
-public struct MusicStage
+public struct MusicState
 {
     public AudioClip clip;
     public int transitionStartTime; // in seconds
@@ -15,9 +16,11 @@ public struct MusicStage
 
 public class MusicManager : MonoBehaviour
 {
-    public MusicStage[] stages;
+    public MusicState[] states;
+    public int timePerBar = 6; // sync transition to bars
     GameSystem system;
-    int currentState;
+    int current;
+    float timer;
 
     /*
      * system.state:
@@ -32,10 +35,48 @@ public class MusicManager : MonoBehaviour
     void Start()
     {
         system = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameSystem>();
+        StartNewState(system.state);
+        current = system.state;
+        timer = 0;
+        StartCoroutine(SyncMusic());
     }
 
-    void Update()
+    IEnumerator SyncMusic()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(timePerBar);
+            CheckState();
+        }
+    }
+
+    void CheckState()
+    {
+        var currentState = states[current - 1];
+        float timePassed = Time.time - timer;
+        if ((current != system.state && timePassed > currentState.transitionStartTime)
+            || timePassed > currentState.transitionEndTime)
+        {
+            FadeCurrentState();
+            StartNewState(system.state);
+            timer = Time.time;
+        }
+    }
+
+    void StartNewState(int state)
+    {
+        var source = gameObject.AddComponent<AudioSource>();
+        source.clip = states[state - 1].clip;
+        source.Play();
+        current = state;
+    }
+
+    void FadeCurrentState()
+    {
+        var source = GetComponent<AudioSource>(); // assume there is only one source in the GameObject
+        source.DOFade(0, states[current - 1].fadeTime).onComplete +=
+            () =>{
+                Destroy(source);
+            };
     }
 }
