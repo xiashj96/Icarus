1f065366d7f52c1e22badfdd49bb7aa86080b31c
王天懿 <wty16@mails.tsinghua.edu.cn>
2019-05-10 03:07:20 +0800

    修改了线的生成方式

--- Assets/Script/LineManager.cs
+++ Assets/Script/LineManager.cs
@@ -9,6 +9,7 @@ public class LineManager : MonoBehaviour
     public float generatePossibility3 = 5f;
     public float generatePossibility;
     float radius = 6.3f;
+    float space = 2f;
 
     GameSystem GS;
 
@@ -21,6 +22,7 @@ public class LineManager : MonoBehaviour
         sun = GameObject.FindGameObjectWithTag("Sun");
         GS = FindObjectOfType<GameSystem>();
         Initialize();
+        StartCoroutine(GenerateCoroutine());
     }
 
     public void Initialize()
@@ -50,17 +52,27 @@ public class LineManager : MonoBehaviour
         generatePossibility = p;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    IEnumerator Generate(float delay, float angle)
     {
-
-        if(Random.Range(0f, 1f) >= generatePossibility * Time.fixedDeltaTime)
-        	return;
-
+        yield return new WaitForSeconds(delay);
         Vector3 sunPos = sun.transform.position;
         GameObject lineObj = Random.Range(0, 2) == 0 ? Instantiate(line1) : Instantiate(line2);
        	lineObj.transform.position = sunPos + Vector3.down * radius;
-       	lineObj.transform.RotateAround(sunPos, Vector3.forward, Random.Range(0f, 360f));
+       	lineObj.transform.RotateAround(sunPos, Vector3.forward, angle);
        	GameObject.Destroy(lineObj, 1.125f);
+    }
+
+    IEnumerator GenerateCoroutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(space);
+            int upperbound = (int)(space * generatePossibility * 2);
+            int cnt = Random.Range(0, upperbound + 1);
+            cnt = Mathf.Min(cnt, 6);
+            float offset = Random.Range(0f, 360f);
+            for(int i = 0; i < cnt; i++)
+                StartCoroutine(Generate(Random.Range(0f, 0.3f), offset + i * 360f / cnt + Random.Range(-15f, 15f)));
+        }
     }
 }
